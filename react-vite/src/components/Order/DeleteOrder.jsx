3a4dbaf1de2473c7ae833543c9373d504d4c76d3
Tyler_Han <tylerhan1226@gmail.com>
2024-03-31 14:42:21 -0700

    cart-actions

--- react-vite/src/components/Order/DeleteOrder.jsx
+++ react-vite/src/components/Order/DeleteOrder.jsx
@@ -5,33 +5,34 @@ import { useEffect } from "react";
 import { deleteOrderThunk, getOrderByIdThunk } from "../../redux/cart";
 // import { deleteInstrumentThunk, getOneInstrumentThunk } from "../../redux/instrument";
 
-export default function DeleteOrder ({orderId, reRenderOnDelete}) {
+export default function DeleteOrder({ orderId}) {
     const dispatch = useDispatch()
     const nav = useNavigate()
     const { closeModal } = useModal()
+    const user = useSelector(state => state.session)
 
     useEffect(() => {
         if (!user) {
             nav('/')
         }
         dispatch(getOrderByIdThunk(orderId))
-    }, [dispatch, orderId])
+    }, [dispatch])
+
 
     const handleDeleteOrder = async (e) => {
         e.preventDefault()
         dispatch(deleteOrderThunk(orderId))
         closeModal()
-        reRenderOnDelete()
         nav(`/orders/MyOrders`)
     }
 
     return (
         <div className='delete-instrument-modal'>
-        <div className='delete-form-container'>
-            <h1 className='remove-inst-title'>Are you sure you want to remove this order from your cart?</h1>
-            <button className='delete-modal-btn confirm-delete-btn' onClick={handleDeleteOrder}>Remove Order Item</button>
-            <button className='delete-modal-btn' onClick={closeModal}>Cancel</button>
+            <div className='delete-form-container'>
+                <h1 className='remove-inst-title'>Are you sure you want to remove this order from your cart?</h1>
+                <button className='delete-modal-btn confirm-delete-btn' onClick={handleDeleteOrder}>Remove Order Item</button>
+                <button className='delete-modal-btn' onClick={closeModal}>Cancel</button>
+            </div>
         </div>
-    </div>
     )
 }
--- react-vite/src/components/Order/OrderOperation.jsx
+++ react-vite/src/components/Order/OrderOperation.jsx
@@ -3,55 +3,65 @@ import { useDispatch } from "react-redux"
 import { deleteOrderThunk, updateOrderThunk } from "../../redux/cart"
 import { useNavigate } from "react-router-dom"
 import DeleteOrder from "./DeleteOrder"
+import OpenModalMenuItem from "../Navigation/OpenModalMenuItem"
 
 
-export default function OrderOperation ({instrument, orderInfo}) {
+export default function OrderOperation({ instrument, orderInfo }) {
 
-    const nav = useNavigate()
     const dispatch = useDispatch()
 
     const [orderQuantity, setOrderQuantity] = useState(orderInfo?.quantity)
-    const [deletedOrder, setDeleteOrder] = useState(false)
-    
-    const reRenderOnDelete = () => {
-        setDeleteOrder(!deletedOrder)
-    } 
-
 
     const handleInc = () => {
         setOrderQuantity(ele => ele + 1)
-        const updatedOrder = {quantity: orderQuantity + 1}
+        const updatedOrder = { quantity: orderQuantity + 1 }
         dispatch(updateOrderThunk(orderInfo.id, updatedOrder))
     }
     const handleDec = () => {
         if (orderQuantity == 1) {
-            return <DeleteOrder />
+            return (
+                <DeleteOrder orderId={orderInfo.id} />
+            )
+        } else {
+            setOrderQuantity(ele => ele - 1)
+            const updatedOrder = { quantity: orderQuantity - 1 }
+            dispatch(updateOrderThunk(orderInfo.id, updatedOrder))
         }
-        setOrderQuantity(ele => ele - 1)
-        const updatedOrder = {quantity: orderQuantity - 1}
-        dispatch(updateOrderThunk(orderInfo.id, updatedOrder))
     }
-    const handleRemoveItem = () => {
-        dispatch(deleteOrderThunk(orderInfo.id))
-        alert('Successfully removed item from cart')
-    }
+    // const handleRemoveItem = () => {
+    //     dispatch(deleteOrderThunk(orderInfo.id))
+    //     alert('Successfully removed item from cart')
+    // }
 
-    // useEffect(() => {
-    //     console.log('Hello!')
-    // }, [orderInfo, orderQuantity])
 
     return (
         <div className="my-cart-item-btn-container">
-        <div className='quantity-container'>
-            <button className="quantity-btn" onClick={handleDec}>-</button>
-            <p>
-                Quantity: {orderQuantity}
-            </p>
-            <button className="quantity-btn" onClick={handleInc}>+</button>
+            <div className='quantity-container'>
+                {orderQuantity == 1 ? (
+                    <button className="quantity-btn" onClick={handleDec}>
+                        <OpenModalMenuItem
+                            // className='cart-btn-text'
+                            itemText='-'
+                            modalComponent={<DeleteOrder orderId={orderInfo.id} />}
+                        />
+                    </button>
+                ) : (
+                    <button className="quantity-btn" onClick={handleDec}>-</button>
+                )}
+                <p className="cart-btn-text">
+                    Quantity: {orderQuantity}
+                </p>
+                <button className="quantity-btn" onClick={handleInc}>+</button>
+            </div>
+            <div className="remove-order-container">
+                <button className="order-action-button">
+                    <OpenModalMenuItem
+                        // className='cart-btn-text'
+                        itemText='Remove from cart'
+                        modalComponent={<DeleteOrder orderId={orderInfo.id} />}
+                    />
+                </button>
+            </div>
         </div>
-        <div className="remove-order-container">
-            <button onClick={handleRemoveItem}>Remove from cart</button>
-        </div>
-    </div>
     )
 }
