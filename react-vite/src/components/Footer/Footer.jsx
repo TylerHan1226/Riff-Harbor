d705672a0bd2ab3a8b8c5bbf40e3be67351887ed
Tyler_Han <tylerhan1226@gmail.com>
2024-04-09 10:44:10 -0700

    update links

--- react-vite/src/components/Footer/Footer.jsx
+++ react-vite/src/components/Footer/Footer.jsx
@@ -11,27 +11,27 @@ export default function Footer() {
             <h2 className="footer-text">Contact</h2>
             <div className="footer-info-container">
                 <div className="footer-info-box">
-                    <NavLink to='https://github.com/TylerHan1226'>
+                    <a href='https://github.com/TylerHan1226'
+                        target="_blank"
+                    >
                         <FaGithub className="footer-icons" />
-                    </NavLink>
+                    </a>
                     <p className="footer-text">TylerHan1226</p>
                 </div>
                 <div className="footer-info-box">
-                    <NavLink to='https://www.linkedin.com/in/yucheng-han-2a3684254/'>
+                    <a href='https://www.linkedin.com/in/yucheng-han-2a3684254/'
+                        target="_blank"
+                    >
                         <FaLinkedin className="footer-icons" />
-                    </NavLink>
+                    </a>
                     <p className="footer-text">Yucheng "Tyler" Han</p>
                 </div>
                 <div className="footer-info-box">
-                    <NavLink>
-                        <MdEmail className="footer-icons" />
-                    </NavLink>
+                    <MdEmail className="footer-icons" />
                     <p className="footer-text">tylerhan1226@gmail.com</p>
                 </div>
             </div>
-            <p>© 2024 All rights reserved by Yucheng "Tyler" Han</p>
-
-
+            {/* <p>© 2024 All rights reserved by Yucheng "Tyler" Han</p> */}
         </div>
-    );
+    )
 }
