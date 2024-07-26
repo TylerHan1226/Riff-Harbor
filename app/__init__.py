bf03d3321c98a360094e0ca1b00fcfc772700a1e
Tyler_Han <tylerhan1226@gmail.com>
2024-07-26 14:13:20 -0700

    fixing http error

--- app/__init__.py
+++ app/__init__.py
@@ -52,8 +52,8 @@ CORS(app)
 @app.before_request
 def https_redirect():
     if os.environ.get('FLASK_ENV') == 'production':
-        if request.headers.get('X-Forwarded-Proto') == 'http':
-            url = request.url.replace('http://', 'https://', 1)
+        if request.headers.get('X-Forwarded-Proto') == 'http' or not request.is_secure:
+            url = request.url.replace('https://', 1)
             code = 301
             return redirect(url, code=code)
 
