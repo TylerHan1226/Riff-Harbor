d922a37d389b9b8783ded435563ec61cf72b20eb
Tyler_Han <tylerhan1226@gmail.com>
2024-08-03 14:15:12 -0700

    nav bar layout

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
@@ -8,76 +8,89 @@ import { MdSailing } from "react-icons/md";
 import { GoHeartFill } from "react-icons/go";
 import OpenModalMenuItem from "./OpenModalMenuItem";
 import SearchModal from "../Search/SearchModal";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
+import { FaSearch } from "react-icons/fa";
 
 export default function Navigation() {
-
+  const nav = useNavigate()
   const user = useSelector(state => state.session.user)
 
   return (
-    <div id="nav">
+    <section className="nav-container">
 
-      <section className="nav-container">
+      <section className='nav-home'>
+        <NavLink to="/">
+          <GiGuitarBassHead id='nav-site-icon' />
+        </NavLink>
+        <h1 id='nav-site-name'>
+          Riff Harbor
+        </h1>
+        <MdSailing className="nav-text-icon" />
+      </section>
 
-        <div id='nav-home'>
-          <NavLink to="/">
-            <GiGuitarBassHead id='nav-icon' />
-          </NavLink>
-          <h1 id='nav-site-name'>
-            Riff Harbor
-          </h1>
-          <MdSailing className="nav-text-icon" />
+      {user &&
+        <section className="nav-action-button-container">
+          <button className="nav-action-button">
+            <NavLink className='nav-action-button-text' to={`/instruments/${user.id}/MyInstruments`}>
+              My Instruments
+            </NavLink>
+          </button>
+          <button className="nav-action-button">
+            <NavLink className='nav-action-button-text' to='/instruments/new'>
+              Sell Your Gear!
+            </NavLink>
+          </button>
+          <button className="nav-action-button">
+            <NavLink className='nav-action-button-text' to='/news/1'>
+              News
+            </NavLink>
+          </button>
+        </section>
+      }
+
+      <div className="nav-icons-container">
+        <div className="nav-search-btn">
+          <OpenModalMenuItem
+            itemText={<FaSearch className='nav-user-action-icon' />}
+            // onItemClick={}
+            className='nav-action-button-text'
+            modalComponent={<SearchModal />}
+          />
         </div>
 
         {user &&
-          <div className="nav-action-button-container">
-            <button className="nav-action-button">
-              <NavLink className='nav-action-button-text' to={`/instruments/${user.id}/MyInstruments`}>
-                My Instruments
-              </NavLink>
-            </button>
-            <button className="nav-action-button">
-              <NavLink className='nav-action-button-text' to='/instruments/new'>
-                Sell Your Gear!
-              </NavLink>
-            </button>
-            <button className="nav-action-button">
-              <NavLink className='nav-action-button-text' to='/news/1'>
-                News
-              </NavLink>
-            </button>
-          </div>
+          <>
+            <NavLink to='/orders/MyOrders'>
+              <FaShoppingCart className='nav-user-action-icon' />
+            </NavLink>
+            <NavLink to='/favorites'>
+              <GoHeartFill className='nav-user-action-icon' />
+            </NavLink>
+            <ProfileButton />
+          </>
         }
+        {!user &&
+          <section className="nav-loginSignup-container">
+            <button className="nav-action-button">
+              <OpenModalMenuItem
+                itemText={"LOG IN"}
+                // onItemClick={}
+                className='nav-action-button-text'
+                modalComponent={<LoginFormModal />}
+              />
+            </button>
+            <button className="nav-action-button">
+              <OpenModalMenuItem
+                itemText={"SIGN UP"}
+                // onItemClick={}
+                className='nav-action-button-text'
+                modalComponent={<SignupFormModal />}
+              />
+            </button>
+          </section>}
+      </div>
 
-        <div className="nav-icons-container">
-          {user &&
-            <>
-              <div className="nav-cart-container">
-                <NavLink to='/orders/MyOrders'>
-                  <FaShoppingCart className='nav-user-action-icon' />
-                </NavLink>
-              </div>
-              <div>
-                <NavLink to='/favorites'>
-                  <GoHeartFill className='nav-user-action-icon' />
-                </NavLink>
-              </div>
-            </>
-          }
-          <div className="nav-profile-container">
-            <ProfileButton className='nav-profile-btn' />
-          </div>
-        </div>
-      </section>
-
-      <section id="nav-search-bar-container">
-          <button className="search-bar-btn">
-            <OpenModalMenuItem 
-              itemText="Search"
-              modalComponent={<SearchModal />}
-            />
-          </button>
-      </section>
-
-    </div>
+    </section>
   );
 }
